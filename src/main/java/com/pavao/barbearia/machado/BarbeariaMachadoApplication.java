23749e5b0eb275be025248d61c8ab48e96a9c6b0
Gustavo Pavao <luisgustavopavao@hotmail.com>
2025-03-07 13:44:13 -0300

    start project

---
+++ src/main/java/com/pavao/barbearia/machado/BarbeariaMachadoApplication.java
@@ -0,0 +1,13 @@
+package com.pavao.barbearia.machado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaMachadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaMachadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pavao/barbearia/machado/BarbeariaMachadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pavao.barbearia.machado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaMachadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
