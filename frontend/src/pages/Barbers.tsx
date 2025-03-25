8660c5dac9167ade4d7c043df190b1f15da3a205
Gustavo Pavão <luisgustavopavao@hotmail.com>
2025-03-25 20:06:38 -0300

    bug fix

--- frontend/src/pages/Barbers.tsx
+++ frontend/src/pages/Barbers.tsx
@@ -1,10 +1,10 @@
 import BarberCard from "../components/BarberCard";
 
 const barbers = [
-    { name: "Gerdson", specialty: "Cortes Clássicos", image: "frontend/src/assets/gerdson.png" },
-    { name: "Junior", specialty: "Degradê e Fade", image: "src/assets/junior.png" },
-    { name: "Gutto", specialty: "Barbas e Navalha", image: "src/assets/gutto.png" },
-    { name: "Pedro", specialty: "Barbas e Navalha", image: "src/assets/pedro.png" },
+    { name: "Gerdson", specialty: "Cortes Clássicos", image: "/images/gerdson.png" },
+    { name: "Junior", specialty: "Degradê e Fade", image: "/public/junior.png" },
+    { name: "Gutto", specialty: "Barbas e Navalha", image: "images/gutto.png" },
+    { name: "Pedro", specialty: "Barbas e Navalha", image: "/pedro.png" },
 ];
 
 export default function Barbers() {
